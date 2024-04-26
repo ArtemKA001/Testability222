21636b7540191515b364fd48ac107b32f9b37744
Neto test <krayzerexe@yandex.ru>
2024-04-26 15:23:33 +0300

    main

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,13 @@ public class Main {
 
     public static void main(String[] args) {
         BmiService service = new BmiService();
-        int weight_KG =98;
+        int weight_KG = 98;
         double height_M = 1.87;
         double metersX2 = height_M * 2;
         int bmi = (int) service.calculate(weight_KG, metersX2);
         System.out.println(bmi);
 
     }
-
-
-
-
-
-
 
 
 }
